3b1aab38fcdf55a8481226e2e1b3b7b3872285b3
Xiang Chen <xiangchenchen96@gmail.com>
2021-11-23 20:42:25 +0100

    :reclycle: Try starlette middleware

--- app/main.py
+++ app/main.py
@@ -1,19 +1,16 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api import profile
 
 
 def create_application() -> FastAPI:
-    application = FastAPI()
-    application.add_middleware(
-        CORSMiddleware,
-        allow_origins=["https://www.mortdogged.com"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    middleware = [
+        Middleware(CORSMiddleware, allow_origins=["https://www.mortdogged.com"])
+    ]
 
+    application = FastAPI(middleware=middleware)
     application.include_router(profile.router, prefix="/profile", tags=["profile"])
 
     return application
