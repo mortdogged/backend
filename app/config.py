2a41eb989f25266b95c7f86ea1768e15db8b62f5
Xiang Chen <xiangchenchen96@gmail.com>
2021-11-28 16:55:45 +0100

    :recycle: Wrap redis client, make it optional

--- app/config.py
+++ app/config.py
@@ -10,8 +10,22 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     api_key: str
     environment: str = "dev"
-    redis_url: str
+    redis_url: str = None
     cache_expired_time: int = 60 * 10
+
+
+class CacheService:
+    def __init__(self, redis_client=None):
+        self.redis_client = redis_client
+
+    def get(self, *args, **kwargs):
+        if self.redis_client:
+            return self.redis_client.get(*args, **kwargs)
+        return None
+
+    def set(self, *args, **kwargs):
+        if self.redis_client:
+            return self.redis_client.set(*args, **kwargs)
 
 
 @lru_cache
@@ -23,4 +37,7 @@ def get_settings() -> Settings:
 
 @lru_cache
 def get_cache():
-    return redis.Redis.from_url(get_settings().redis_url)
+    redis_url = get_settings().redis_url
+    if not redis_url:
+        return CacheService()
+    return CacheService(redis.Redis.from_url(redis_url))
